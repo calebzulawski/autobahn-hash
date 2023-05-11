7e5494ae131e85c38f62e740027b1ddf5d1edd06
Caleb Zulawski <caleb.zulawski@gmail.com>
2023-05-11 11:55:57 -0400

    Implement Hasher

--- src/lib.rs
+++ src/lib.rs
@@ -55,17 +55,14 @@ fn remainder(bytes: &[u8]) -> [u8; 32] {
     packet
 }
 
-fn bytes_as_packet(bytes: [u8; 32]) -> [u64; 4] {
-    let mut packet = [0; 4];
-    for (i, chunk) in bytes.chunks(8).enumerate() {
-        packet[i] = u64::from_le_bytes(chunk.try_into().unwrap());
-    }
-    packet
-}
-
 impl AutobahnHash {
+    /// Create a new `AutobahnHash`.
+    pub fn new() -> Self {
+        Self::new_with_key([0; 4])
+    }
+
     /// Create a new `AutobahnHash` with the given key.
-    pub fn new(key: [u64; 4]) -> Self {
+    pub fn new_with_key(key: [u64; 4]) -> Self {
         let key = u64x4::from_array(key);
         let mul0 = u64x4::from_array([
             0xdbe6d5d5fe4cce2f,
@@ -85,7 +82,7 @@ impl AutobahnHash {
     }
 
     /// Write a packet of data to the hasher.
-    pub fn write(&mut self, packet: [u64; 4]) {
+    pub fn write_packet(&mut self, packet: [u64; 4]) {
         let packet = u64x4::from_array(packet);
         self.v1 += self.mul0 + packet;
         self.mul0 ^= (self.v1 & u64x4::splat(0xffff_ffff)) * (self.v0 >> u64x4::splat(32));
@@ -93,6 +90,15 @@ impl AutobahnHash {
         self.mul1 ^= (self.v0 & u64x4::splat(0xffff_ffff)) * (self.v1 >> u64x4::splat(32));
         self.v0 += zipper_merge(self.v1);
         self.v1 += zipper_merge(self.v0);
+    }
+
+    /// Write a packet of data to the hasher, in the form of bytes.
+    pub fn write_bytes(&mut self, bytes: [u8; 32]) {
+        let mut packet = [0; 4];
+        for (i, chunk) in bytes.chunks(8).enumerate() {
+            packet[i] = u64::from_le_bytes(chunk.try_into().unwrap());
+        }
+        self.write_packet(packet);
     }
 
     fn finish(&mut self, remainder: &[u8]) {
@@ -110,17 +116,20 @@ impl AutobahnHash {
             let size = remainder.len() as u64;
             self.v0 += u64x4::splat((size << 32) + size);
             rotate_32_by(size, &mut self.v1);
-            self.write(bytes_as_packet(self::remainder(remainder)));
+            self.write_bytes(self::remainder(remainder));
         }
     }
 
     /// Produce a `u64` hash.
     ///
     /// The `remainder` bytes must be less than a packet (less than 32 bytes).
+    ///
+    /// Writing the remainder is notably different than `Hasher::write`.  The remainder is padded
+    /// and permuted into a 32-bit packet.
     pub fn finish_u64(mut self, remainder: &[u8]) -> u64 {
         self.finish(remainder);
         for _ in 0..4 {
-            self.write(permute(self.v0).to_array());
+            self.write_packet(permute(self.v0).to_array());
         }
         self.v0[0]
             .wrapping_add(self.v1[0])
@@ -129,15 +138,34 @@ impl AutobahnHash {
     }
 }
 
+impl core::hash::Hasher for AutobahnHash {
+    fn finish(&self) -> u64 {
+        self.clone().finish_u64(&[])
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        // `Hasher` requires calls to be exactly sequenced (e.g. two calls to `write` does not need
+        // to be the same as a single call two `write` with the same data concatenated).
+        // Therefore, we don't need to buffer and can simply pad bytes.
+        let (bytes, remainder) = bytes.split_at(bytes.len() / 32 * 32);
+        for packet in bytes.chunks(32) {
+            self.write_bytes(packet.try_into().unwrap())
+        }
+        let mut packet = [0; 32];
+        packet[..remainder.len()].copy_from_slice(remainder);
+        self.write_bytes(packet);
+    }
+}
+
 /// Hash a slice with the given key.
 ///
 /// This function automatically dispatches to the optimal instruction set.
 #[multiversion(targets = "simd")]
 pub fn hash_u64(bytes: &[u8], key: [u64; 4]) -> u64 {
-    let mut hasher = AutobahnHash::new(key);
+    let mut hasher = AutobahnHash::new_with_key(key);
     let (bytes, remainder) = bytes.split_at(bytes.len() / 32 * 32);
     for packet in bytes.chunks(32) {
-        hasher.write(bytes_as_packet(packet.try_into().unwrap()));
+        hasher.write_bytes(packet.try_into().unwrap());
     }
     hasher.finish_u64(remainder)
 }
